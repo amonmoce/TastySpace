1089a8a8396ddf69146196699f1fed55e58aa530
Amon <amonmoce@yahoo.fr>
2018-10-01 23:27:44 +0800

    added code for dialogflow response

--- __init__.py
+++ __init__.py
@@ -1,11 +1,22 @@
 from flask import Flask, Response
-
+from flask import jsonify
+from flask import make_response
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return Response("It works!"), 200
+    if request.method == 'GET':
+        return Response("TastySpace is up and running!"), 200
+    else:
+        req = request.get_json(force=True)
+        intent_name = req.get('queryResult').get('intent').get('displayName')
+        fulfillmentText = req.get('queryResult').get('fulfillmentText')
+        response = {
+            "fulfillmentText": fulfillmentText + '(' + intent_name + ')',
+        }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
